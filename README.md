77f64ecfc19e2b95e1ad5ce47d42684ddbb0dc04
Scott Simpson <scottincrypto@gmail.com>
2022-03-16 08:49:36 +0000

    Tidy up code & rename project

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# dune-rari-fuse-assets
-Creates the Rari Fuse contracts abstraction for Dune Analytics
+# Rari Fuse Assets for Dune Analytics
+
+There isn't enough information in on-chain calls & events to easily get a full picture of Rari Fuse pools on Dune Analytics.  This eth-brownie script queries the Rari Fuse contracts on Ethereum mainnet and creates a [Dune User-Generated Table](https://docs.dune.xyz/data-tables/data-tables/user-generated) SQL file containing the Fuse Pool, Fuse fToken and underlying token details.
+
+
+## Prerequisites
+
+Please install or have installed the following:
+
+- [nodejs and npm](https://nodejs.org/en/download/)
+- [python](https://www.python.org/downloads/)
+## Installation
+
+1. [Install Brownie](https://eth-brownie.readthedocs.io/en/stable/install.html), if you haven't already. Here is a simple way to install brownie.
+
+```bash
+pip install eth-brownie
+```
+Or, if that doesn't work, via pipx
+```bash
+pip install --user pipx
+pipx ensurepath
+# restart your terminal
+pipx install eth-brownie
+```
+
+2. [Install ganache-cli](https://www.npmjs.com/package/ganache-cli)
+
+```bash
+npm install -g ganache-cli
+```
+
+3. Set up Brownie Environment
+
+Configure .env file the rari_fuse_sql folder with an Infura or Alchemy API ID, and an etherscan API id:
+
+```
+export ETHERSCAN_TOKEN=<your etherscan api id>
+export WEB3_ALCHEMY_PROJECT_ID=<your api id>
+#or
+export WEB3_INFURA_PROJECT_ID=<your api id>
+```
+
+If using Alchemy, switch to Alchemy providers using:
+
+```bash
+brownie networks set_provider alchemy
+```
+
+## Quickstart
+
+
+1. Clone this repo
+
+```bash
+git clone https://github.com/scottincrypto/dune-rari-fuse-assets
+```
+
+2. Run the script
+
+```
+cd rari_fuse_sql
+brownie run fusepools
+```
+
+3. Execute the SQL
+
+The contents of the resulting output file rari.sql can be copy/pasted into Dune Analytics to refresh the token list
--- rari_fuse_sql/scripts/fusepools.py
+++ rari_fuse_sql/scripts/fusepools.py
@@ -10,7 +10,6 @@ def main():
     """main"""
     all_assets = get_all_assets()
     query = generate_sql_query(all_assets)
-    #print(query)
 
     with open('rari.sql', 'w') as f:
         f.write(query)
@@ -18,78 +17,69 @@ def main():
 
 
 def get_all_assets():
-    """Gets the pools from the FusePoolDirectory and gets the assets from each pool
-        Takes an 
+    """Gets the pools from the FusePoolDirectory and gets the assets from each pool.
+        The assets are returned as a list of tuples, which is appended to the existing list.
+        Contract objects are stored as an alias to persist between sessions for speed up on subsequent runs.
     """
-    #Contract('FusePoolDirectory').set_alias(alias=None)
+    #Contract('FusePoolDirectory').set_alias(alias=None) #uncomment to clear contract aliases for troubleshooting
     try: 
         Contract('FusePoolDirectory').alias
     except ValueError:
-        #print(sys.exc_info()[0])
         Contract.from_explorer('0x835482FE0532f169024d5E9410199369aAD5C77E').set_alias('FusePoolDirectory', persist=True)
-        #print('getting directory contract')
 
     fuse_pool_directory = Contract('0x835482FE0532f169024d5E9410199369aAD5C77E')
     all_pools = fuse_pool_directory.getAllPools()
 
     index = 0
     pool_start = 0 #skip pools & start here
-    pool_count = 10 #stop before index for this many pools
+    pool_count = 10000 #stop before index for this many pools
     all_assets = []
     for pool in all_pools:
         if index < pool_count and index >= pool_start:
             assets_return_val = get_assets_from_pool(index, pool[0], pool[2])
             all_assets += assets_return_val
-        
         index += 1
-
-    
-    #print(all_pools[6][2])
-    #get_assets(6, all_pools[6][0], all_pools[6][2])
-
-    #for asset in all_assets:
-    #    print(asset)
 
     return all_assets
 
+
+
 def get_assets_from_pool(pool_index, pool_name, comptroller):
-    """gets the assets from a pool"""
-
-
-    #using a local copy for speed & convenience.  Retrieved from etherscan '0xE16DB319d9dA7Ce40b666DD2E365a4b8B3C18217'
-    comptroller_abi = json.loads(open('scripts/comptroller.json').read())
-    #Contract('comptroller_' + comptroller).set_alias(alias=None)
+    """Gets the assets from the pool specified by comptroller.
+        Returns a list of tuples, with each element containing the details of an asset in a pool.
+        Contract objects are stored as an alias to persist between sessions for speed up on subsequent runs.
+    """
+    #using a local copy of abi for speed & convenience.  Retrieved from etherscan '0xE16DB319d9dA7Ce40b666DD2E365a4b8B3C18217'
+    comptroller_abi = json.loads(open('interfaces/comptroller.json').read())
+    #Contract('comptroller_' + comptroller).set_alias(alias=None)  #uncomment to clear contract aliases for troubleshooting
     try: 
         Contract('comptroller_' + comptroller).alias
     except ValueError:
         Contract.from_abi('Comptroller', comptroller, comptroller_abi).set_alias('comptroller_' + comptroller, persist=True)
-        #print('getting lens contract')
     
 
-    #initialise the comptroller contract & grab the list of pool assets
+    # nitialise the comptroller contract & grab the list of pool assets
     pool_contract = Contract('comptroller_' + comptroller)
-    #print('comptroller - ' + comptroller)
+
     fuse_pool_assets = pool_contract.getAllMarkets()
-    #print(fuse_pool_assets)
-    #using a local copy for speed & convenience.  Retrieved from etherscan 
-    cerc20_abi = json.loads(open('scripts/CErc20Delegate.json').read())
+
+    #using a local copy for speed & convenience.  Retrieved from etherscan '0x67Db14E73C2Dce786B5bbBfa4D010dEab4BBFCF9'
+    cerc20_abi = json.loads(open('interfaces/CErc20Delegate.json').read())
     #print(cerc20_abi)
 
     #loop through the data & store in list of tuples
     return_list = []
     for asset in fuse_pool_assets:
-        #Contract('token_' + asset[0]).set_alias(alias=None) 
-        #print(asset)
+        #Contract('token_' + asset[0]).set_alias(alias=None)  #uncomment to clear contract aliases for troubleshooting
         
         #get the ftoken object
         try: 
             Contract('token_' + asset).alias
         except ValueError:
-            #using abi as etherscan isn't reliably detecting asset tokens as proxy contracts
+            #using local abi as etherscan isn't reliably detecting asset tokens as proxy contracts
             Contract.from_abi('CErc20Delegate', asset, cerc20_abi).set_alias('token_' + asset, persist=True)
 
         ftoken_contract = Contract('token_' + asset)
-        #print('ftoken - ' + asset)
         
         #get the underlying token
         underlying = ftoken_contract.underlying()
@@ -97,18 +87,19 @@ def get_assets_from_pool(pool_index, pool_name, comptroller):
         #change out wETH for ETH so that ERC20 methods work
         if underlying == '0x0000000000000000000000000000000000000000':
             underlying = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-        #print('underlying - ' + underlying)
+
         try: 
             Contract('token_' + underlying).alias
         except ValueError:
-            #lazily using cERC20 for underlying because it implements .name, .decimals, .symbol correctly
+            #lazily using CErc20Delegate for underlying because it implements .name, .decimals, .symbol 
             Contract.from_abi('CErc20Delegate', underlying, cerc20_abi).set_alias('token_' + underlying, persist=True)
 
         underlying_contract = Contract('token_' + underlying)
-        # element schema (pool_index, pool_name, comptroller, asset_address, asset_name, asset_symbol
+        # element schema (pool_index, pool_name, comptroller, asset_address, asset_name, asset_symbol \
         #                   asset_decimals, underlying_address, underlying_name, underlying_symbol, underlying_decimals)
         
-        if underlying == '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2':  #special case for MKR, non-ERC20 compliant causes int overflow
+        #special case for MKR, non-ERC20 compliant causes int overflow as expected strings are stored as byte32
+        if underlying == '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2':  
             element = (pool_index, pool_name, comptroller, asset, ftoken_contract.name(), \
                 ftoken_contract.symbol(), ftoken_contract.decimals(), underlying, \
                 'Maker', 'MKR', underlying_contract.decimals())
@@ -127,7 +118,7 @@ def get_assets_from_pool(pool_index, pool_name, comptroller):
 
 
 def debug_tokens():
-    """docstring"""
+    """Used to debug a troublesome token"""
     asset = '0x85b294139E77E7dE519A9bA9553D274d79E4812e'
     try: 
         Contract('token_' + asset).alias
@@ -158,15 +149,13 @@ def debug_tokens():
     print(ftoken_contract.name())
     print(ftoken_contract.symbol())
     print(ftoken_contract.decimals())
-    print(underlying)
-    val = underlying_contract.name().astype(str)
-    print(val)
-    #print(underlying_contract.symbol())
-    #print(underlying_contract.decimals())
+    print(underlying_contract.name())
+    print(underlying_contract.symbol())
+    print(underlying_contract.decimals())
 
 
 def generate_sql_query(assets):
-    """Takes a list of tuples & translates to SQL text for use in Dune Analytics query"""
+    """Takes a list of tuples & formats to SQL text for use in Dune Analytics query"""
 
     sql_query_string = 'CREATE OR REPLACE VIEW dune_user_generated.rari_capital_fuse_ftokens (pool_index, pool_name, comptroller, ftoken_address, ftoken_name, ftoken_symbol, '
     sql_query_string += 'ftoken_decimals, underlying_address, underlying_name, underlying_symbol, underlying_decimals) AS VALUES \n'
